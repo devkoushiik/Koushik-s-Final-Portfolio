3e0952bf423e5664187ddc3331fea4adb0dff01a
koushikxahmed <koushikxahmed@gmail.com>
2024-08-10 18:37:47 +0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
-            className="mt-[11px] md:mt-[17.5px] md:text-lg lg:mt-[65px]"
+            className="mt-[13px] md:mt-[17.5px] md:text-lg lg:mt-[65px]"
           >
             {HERO.lastName}
           </motion.span>
